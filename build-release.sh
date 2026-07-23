1ffc294915ae4f947b7e894427374cb5db4819a3
shashsinha8 <shashsinha.us@gmail.com>
2026-07-23 13:52:23 -0500

    Disable nested Swift compiler sandbox in release builds

--- build-release.sh
+++ build-release.sh
@@ -23,7 +23,9 @@ done
 ./script/check-uncommitted-files.sh
 ./generate.sh --build-version "$build_version" --codesign-identity "$codesign_identity" --generate-git-hash
 
-swift build -c release --arch arm64 --arch x86_64 --product aerospace -Xswiftc -warnings-as-errors # CLI
+swift build -c release --arch arm64 --arch x86_64 --product aerospace \
+    -Xswiftc -warnings-as-errors \
+    -Xswiftc -disable-sandbox # CLI
 
 # todo: make xcodebuild use the same toolchain as swift
 # toolchain="$(plutil -extract CFBundleIdentifier raw ~/Library/Developer/Toolchains/swift-6.1-RELEASE.xctoolchain/Info.plist)"
